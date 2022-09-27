b91b68923aa783de1339fe5a2bc9069e71f2cbd7
dragec8 <dragutin.oreski@gmail.com>
2022-09-27 09:34:28 +0200

    feat: extract logic for browsing seen to a function

--- pokedex/buttons.py
+++ pokedex/buttons.py
@@ -2,6 +2,9 @@ from gpiozero import Button
 import subprocess
 import sys
 
+from pokedex.display import show_dex_image
+from pokedex.resources import get_seen_names
+
 button1 = Button(22)
 button2 = Button(23)
 button3 = Button(24)
@@ -10,3 +13,26 @@ button3 = Button(24)
 def run_program(program, exit_code=0):
     subprocess.Popen(program)  # Start the dex
     sys.exit(exit_code)  # close this script
+
+
+def browse_seen():
+    seen_names = get_seen_names()
+    size = len(seen_names)
+    index = 0
+
+    while 0 < len(seen_names):
+
+        show_dex_image(seen_names[index])
+
+        if button2.is_pressed:
+            index = (index + 1) % size
+            show_dex_image(seen_names[index])
+            print(index)
+
+        if button3.is_pressed:
+            index = (index - 1) % size
+            show_dex_image(seen_names[index])
+            print(index)
+
+        if button1.is_pressed:
+            return
