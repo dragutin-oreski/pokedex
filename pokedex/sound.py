afdb4f7dbe7963bbac405f18bb0d64b78ce5ded6
dragec8 <dragutin.oreski@gmail.com>
2022-09-29 02:28:38 +0200

    fix: remove the condition to only read text if the file is not seen

--- pokedex/sound.py
+++ pokedex/sound.py
@@ -10,8 +10,6 @@ cmd_voice = '-ven+m5 '  # Assigns which voice ill be using
 
 def text_to_speech(found_class):
     seen_file = get_seen_file(found_class)
-    if os.path.isfile(seen_file):
-        return
     dex_entry = get_description_from_dex(found_class)
     subprocess.call(
         [cmd_beg + cmd_voice + dex_entry + cmd_end],
