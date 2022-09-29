5f5fa1b8b45b32b4e6c3def414039834aa85e17a
dragec8 <dragutin.oreski@gmail.com>
2022-09-29 02:39:06 +0200

    fix: remove unused seen_file

--- pokedex/sound.py
+++ pokedex/sound.py
@@ -1,7 +1,6 @@
-import os
 import subprocess
 
-from pokedex.resources import get_description_from_dex, get_seen_file
+from pokedex.resources import get_description_from_dex
 
 cmd_beg = 'sudo espeak -s160 '  # puts sudo espeak in term
 cmd_end = ' 2>/dev/null'  # cleans up the output from the terminal
@@ -9,7 +8,6 @@ cmd_voice = '-ven+m5 '  # Assigns which voice ill be using
 
 
 def text_to_speech(found_class):
-    seen_file = get_seen_file(found_class)
     dex_entry = get_description_from_dex(found_class)
     subprocess.call(
         [cmd_beg + cmd_voice + dex_entry + cmd_end],
